1c3f7ba00296b0ed48af4c4f047cb6d0575483b1
eguaman1990 <es.aguaman@gmail.com>
2014-10-16 07:39:49 -0300

    Merge origin/master
    Conflicts:
    	GCamas-ejb/src/main/java/cl/hblt/sessions/BussinessFacade.java
    	GCamas-ejb/src/main/java/cl/hblt/sessions/BussinessFacadeLocal.java
    	GCamas-ejb/src/main/resources/META-INF/persistence.xml

--- Fabrimed/index.php
+++ Fabrimed/index.php
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -10,6 +11,20 @@
 <iframe width="640" height="480" src="//www.youtube.com/embed/oavMtUWDBTM?autoplay=1" frameborder="0" allowfullscreen></iframe>
 </body>
 </html>
+=======
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "bienvenido";
+    ?>
+  </body>
+</html>
+>>>>>>> origin/master
 =======
 <!DOCTYPE html>
 <html>
